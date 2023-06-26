c848b156a7128a11197d0b8e428689d39026409a
gadnlino <guiavenas@gmail.com>
2023-06-25 23:43:00 -0300

    adicionando exemplo com a rede vgg19

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from cifar10 import Cifar10
 import tensorflow as tf
 from tensorflow.python.keras import metrics
+from keras.applications import VGG19, ResNet50
 
 # https://keras.io/api/metrics/
 METRICS = [
@@ -9,12 +10,14 @@ METRICS = [
     metrics.Recall(),
 ]
 
+INPUT_SHAPE = (32, 32, 3)
+
 
 def model_example():
     #https://www.kaggle.com/code/amyjang/tensorflow-cifar10-cnn-tutorial/notebook
 
     model = tf.keras.models.Sequential([
-            tf.keras.layers.Conv2D(32, 3, padding='same', input_shape=(32, 32, 3), activation='relu'),
+            tf.keras.layers.Conv2D(32, 3, padding='same', input_shape=INPUT_SHAPE, activation='relu'),
             tf.keras.layers.Conv2D(32, 3, activation='relu'),
             tf.keras.layers.MaxPooling2D(),
             tf.keras.layers.Dropout(0.25),
@@ -40,7 +43,7 @@ def model_sigmoid():
     # Igual ao model_example, somente trocando a função de ativação para sigmoid
     # Com sigmoid, o modelo não performa bem, acurácia fica baixa
     model = tf.keras.models.Sequential([
-            tf.keras.layers.Conv2D(32, 3, padding='same', input_shape=(32, 32, 3), activation='sigmoid'),
+            tf.keras.layers.Conv2D(32, 3, padding='same', input_shape=INPUT_SHAPE, activation='sigmoid'),
             tf.keras.layers.Conv2D(32, 3, activation='sigmoid'),
             tf.keras.layers.MaxPooling2D(),
             # tf.keras.layers.Dropout(0.25),
@@ -64,7 +67,28 @@ def model_sigmoid():
 
 def model_vgg19():
     #https://www.kaggle.com/code/adi160/cifar-10-keras-transfer-learning/notebook
-    pass
+    
+    base_model_1 = VGG19(include_top=False,weights='imagenet',input_shape=INPUT_SHAPE,classes=10)
+
+    model_1= tf.keras.models.Sequential()
+    model_1.add(base_model_1) #Adds the base model (in this case vgg19 to model_1)
+    model_1.add(tf.keras.layers.Flatten())
+    model_1.add(tf.keras.layers.Dense(1024,activation=('relu'),input_dim=512))
+    model_1.add(tf.keras.layers.Dense(512,activation=('relu'))) 
+    model_1.add(tf.keras.layers.Dense(256,activation=('relu'))) 
+    #model_1.add(Dropout(.3))#Adding a dropout layer that will randomly drop 30% of the weights
+    model_1.add(tf.keras.layers.Dense(128,activation=('relu')))
+    #model_1.add(Dropout(.2))
+    model_1.add(tf.keras.layers.Dense(10,activation=('softmax')))
+
+    learn_rate=.001
+
+    sgd=tf.keras.optimizers.SGD(lr=learn_rate,momentum=.9,nesterov=False)
+    #adam=tf.keras.optimizers.Adam(lr=learn_rate, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+
+    model_1.compile(optimizer=sgd,loss='categorical_crossentropy',metrics=METRICS)
+
+    return model_1
 
 def model_resnet():
     #https://www.kaggle.com/code/adi160/cifar-10-keras-transfer-learning/notebook
@@ -73,4 +97,4 @@ def model_resnet():
 if(__name__ == "__main__"):
     cifar = Cifar10()
     #cifar.run()
-    cifar.run_training(model=model_example(), epochs = 2, batch_size = 256, shuffle=True)
+    cifar.run_training(model=model_vgg19(), epochs = 10, batch_size = 500, shuffle=True)
